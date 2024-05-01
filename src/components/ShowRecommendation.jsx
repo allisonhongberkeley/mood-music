e383d5f0511894f72bfe7a24695415ae12e8b16b
Allison Hong <allisonhong1023@berkeley.edu>
2024-05-01 01:16:30 -0700

    add API call

--- src/components/ShowRecommendation.jsx
+++ src/components/ShowRecommendation.jsx
@@ -1,50 +1,76 @@
 import {React, useState, useEffect} from 'react';
-import { useNavigate } from "react-router-dom";
 import Navbar from './Navbar'
 import { useLocation } from 'react-router-dom';
-import Feature from './Feature'
 import './Feature.css'
 
 
 function ShowRecommendation() {
     const location = useLocation();
     const { data } = location.state || {};
+    let isLoaded = false;
     const [recommendations, setRecommendations] = useState([]);
 
-    useEffect(() => {
-         const fetchData = async () => {
-             try {
-    //             // Construct query parameters based on the selected features
-    //             const queryParams = new URLSearchParams();
-                 Object.entries(data).forEach(([feature, { min, max }]) => {
-                     queryParams.append(feature.toLowerCase(), `${min},${max}`);
-                 });
+    const fetchData = async () => {
+      try {
+          const min_danceability  = (data.Loudness.min * 0.1).toFixed(1);
+          const max_danceability = (data.Loudness.max * 0.1).toFixed(1);
+          const min_energy = (data.Energy.min * 0.1).toFixed(1);
+          const max_energy = (data.Energy.max * 0.1).toFixed(1);
+          const min_acousticness = (data.Acousticness.min * 0.1).toFixed(1);
+          const max_acousticness = (data.Acousticness.max * 0.1).toFixed(1);
+          const min_instrument = (data.Instrumentalness.min * 0.1).toFixed(1);
+          const max_instrument = (data.Acousticness.max * 0.1).toFixed(1);
+          const min_liveness = data.Liveness.min * 0.1;
+          const max_liveness = data.Liveness.max * 0.1;
+          const min_loudness = (data.Loudness.min * 0.1).toFixed(1);
+          const max_loudness = (data.Loudness.max * 0.1).toFixed(1);
+          const min_tempo = (data.Tempo.min * 0.1).toFixed(1);
+          const max_tempo = (data.Tempo.max * 0.1).toFixed(1);
 
-    //             // Make a request to the Spotify API
-    //             const response = await fetch(`https://api.spotify.com/v1/recommendations?${queryParams.toString()}`, {
-    //                 method: 'GET',
-    //                 headers: {
-    //                     'Authorization': 'Bearer YOUR_ACCESS_TOKEN',
-    //                     'Content-Type': 'application/json'
-    //                 },
-    //             });
+         const seedGenres = ["classical","country","pop","world-music"]
+         const seedArtist = "4NHQUGzhtTLFvgF5SZesLK";
 
-    //             if (!response.ok) {
-    //                 throw new Error('Failed to fetch recommendations');
-    //             }
+         isLoaded = true;
+         console.log(isLoaded);
 
-    //             const responseData = await response.json();
-    //             setRecommendations(responseData.tracks || []);
-             } catch (error) {
-                 console.error('Error fetching recommendations:', error.message);
-             }
-         };
-
-         if (data) {
-             fetchData();
-         }
+       //   const response = await fetch(`https://api.spotify.com/v1/recommendations?seed_artists${seedArtist}&seed_genres=${seedGenres}&
+       //     min_acousticness=${min_acousticness}&max_acousticness=${max_acousticness}&
+       //     min_energy=${min_energy}&max_energy=${max_energy}&
+       //     min_tempo=${min_tempo}&max_energy=${max_tempo}&
+       //     min_loudness=${min_loudness}&max_loudness=${max_loudness}&
+       //     min_liveness=${min_liveness}&max_liveness=${max_liveness}&
+       //     min_instrument=${min_instrument}&max_instrument=${max_instrument}&
+       //     min_danceability=${min_danceability}&max_danceability=${max_danceability}&
+       //     limit=1`, {
+       //     method: 'GET',
+       //     headers: {
+       //         'Authorization': 'Bearer ACCESS_TOKEN',
+       //         'Content-Type': 'application/json'
+       //     }
+       // });
          
-    }, [data]);
+
+       //    if (!response.ok) {
+       //        throw new Error('Failed to fetch recommendations');
+       //    }
+
+       //    const responseData = await response.json();
+       //    const trackName = responseData.tracks[0].album.name;
+       //    const artistName = responseData.tracks[0].album.artists[0].name;
+       //    console.log(trackName);
+       //    console.log(artistName);
+       //    setRecommendations([trackName, artistName] || []);
+       //    setisLoaded(true);
+      } catch (error) {
+          console.error('Error fetching recommendations:', error.message);
+      };
+  }
+
+  useEffect(() => {
+    if (!isLoaded) {
+        fetchData();
+    }
+  }, []);
 
 
       return (
@@ -56,7 +82,8 @@ function ShowRecommendation() {
                 {feature}: {min} - {max}
               </ul>
             ))}
-        <h2>your song rec is:</h2>
+        {isLoaded && <h2>your song rec is: {`${recommendations[0]} by ${recommendations[1]}`}</h2>}
+        
 
         </div>
       );
